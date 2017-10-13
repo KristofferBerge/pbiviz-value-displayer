152717a12687bf42b4c3708449fa4be9025e5139
Kristoffer Berge <krberge@microsoft.com>
2017-10-13 15:43:19 +0200

    fixing .gitignore

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var valueDisplayerAA1089073DAF4BAF951DD0E63B3BB01F = {
+        name: 'valueDisplayerAA1089073DAF4BAF951DD0E63B3BB01F',
+        displayName: 'Value Displayer',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.8.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.valueDisplayerAA1089073DAF4BAF951DD0E63B3BB01F.Visual(options),
+        custom: true
+    };
+}
